d66dd07e006f01d3a723f6fd6796aceb16474b56
crixstn <crixstn@gmail.com>
2023-01-16 09:57:54 -0300

    feat: POST /messages

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
+import dayjs from "dayjs"
 
 dotenv.config()
 
@@ -19,6 +20,16 @@ try {
 const app = express()
 app.use(cors())
 app.use(json())
+
+app.get("/participants", (req, res) => {
+
+    db.collection("participants").find().toArray().then(
+        datas => {
+            return res.send(datas) 
+    }).catch((err) => {
+        res.status(500).send(`:( Error: ${err}`)
+    })
+})
 
 app.post("/participants", async (req, res) => {
     const user = req.body
@@ -39,26 +50,66 @@ app.post("/participants", async (req, res) => {
             return res.status(409).send("This user already exist")
         }
 
+
         const newUser = {
             name: user.name,
             lastStatus: Date.now()
         }
 
+        const newUserMessage = {
+            from: user.name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: dayjs(newUser.lastStatus).format("HH:mm:ss")
+        }
+
         await db.collection("participants").insertOne(newUser)
+        await db.collection("messages").insertOne(newUserMessage)
         res.sendStatus(201)
     }catch (err){
         res.sendStatus(500)
     }
 })
 
-app.get("/participants", (req, res) => {
+app.post("/messages", async (req, res) => {
+    const { to, type, text } = req.body
+    const { user } = req.headers
 
-    db.collection("participants").find().toArray().then(
-        datas => {
-            return res.send(datas) 
-    }).catch((err) => {
-        res.status(500).send(`:( Error: ${err}`)
-    })
+    try{
+        const existUser = await db.collection("participants").findOne({ name: user })
+        if(!existUser) {
+           return res.status(422).send("ocorreu um erro")
+        }
+
+        const newMessage = {
+            from: existUser.name,
+            to,
+            text,
+            type,
+            time: dayjs(Date.now()).format("HH:mm:ss")
+        }
+
+        const messageSchema = joi.object({
+            from: joi.string().required(),
+            to: joi.string().required(),
+            text: joi.string().required(),
+            type: joi.string().valid("message", "private_message").required(),
+            time: joi.string().required()
+        })
+
+        const validation = messageSchema.validate(newMessage, {abortEarly: false})
+        if(validation.error) {
+            const err = validation.error.details.map( item => item.message)
+            return res.status(422).send(err)
+        }
+
+        await db.collection("messages").insertOne(newMessage)
+        res.sendStatus(201)
+        console.log(newMessage)
+    }catch(err) {
+        res.sendStatus(500)
+    }
 })
 
 const PORT = 5000
